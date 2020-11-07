9348ee50a53bf363b63e8e4be5599a583ba331e5
JIMMY JACOB <68079806+jimjacob29@users.noreply.github.com>
2020-11-07 17:35:23 +0530

    change

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-
 function handleServer(req, res) {
-  
+  if (req.url === "/welcome") {
+    res.write("Welcome to Dominos!");
+    res.end();
+  }
+  if (req.url === "/welcome") {
+    res.write(
+      JSON.stringify({
+        phone: "18602100000",
+        email: "guestcaredominos@jublfood.com"
+      })
+    );
+    res.end();
+  } else {
+    res.status(404);
+  }
 }
-
+httpServer.listen(8081);
 module.exports = httpServer;
