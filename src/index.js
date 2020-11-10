a8b98a04adb5f45c8ab45867a77e8db0dca836a6
JIMMY JACOB <68079806+jimjacob29@users.noreply.github.com>
2020-11-10 12:46:01 +0530

    change

--- src/index.js
+++ src/index.js
@@ -4,21 +4,19 @@ const httpServer = http.createServer(handleServer);
 
 function handleServer(req, res) {
   if (req.url === "./welcome") {
-    // res.statusCode = 200;
-    res.write("Welcome to Dominos!");
-
-    res.end();
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Welcome to Dominos!");
   }
   if (req.url === "./contact") {
-    // res.statusCode = 200;
-    res.write(
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(
       JSON.stringify({
         phone: "18602100000",
         email: "guestcaredominos@jublfood.com"
       })
     );
-
-    res.end();
   } else {
     res.statusCode = 404;
     res.end();
