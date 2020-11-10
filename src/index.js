aaf37b0d12e68110e85f9b6f5b7bfd4c93763300
JIMMY JACOB <68079806+jimjacob29@users.noreply.github.com>
2020-11-10 12:48:38 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-var http = require("http");
+const http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
 function handleServer(req, res) {
-  if (req.url === "./welcome") {
+  if (req.url === "/welcome") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end("Welcome to Dominos!");
   }
-  if (req.url === "./contact") {
+  if (req.url === "/contact") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
     res.end(
